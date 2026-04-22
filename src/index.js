2a78846df28d424189e69beb6d3baa452dfdcf76
andre-violin <andre.violin@ifms.edu.br>
2026-04-22 15:17:44 -0400

    chore: add express and prisma config

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+
+// Configuração inicial
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5501;
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.json({ status: "OK", timestamp: new Date().toISOString() });
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "🚀 MinURL API rodando!",
+    version: "0.1.0",
+    endpoints: {
+      health: "/health",
+    },
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor em http://localhost:${PORT}`);
+});
